8d9055ee4bff7e9fe1d371d92c1d04c93010d642
acarana <acarana@gmail.com>
2019-03-04 15:42:46 -0400

    Add files via upload

---
+++ token.h
@@ -0,0 +1,34 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+#include <iostream>
+#include <string>
+#include <map>
+#include "Reserved.h"
+using namespace std;
+
+
+
+static States State;
+static map<string,Keywords> KeyMap;
+static map<string,Keywords>::iterator ITE;
+
+struct token
+{
+    TokenTypes TokenType;
+    string TokenString;
+
+    union
+    {
+        Keywords keyID;
+        int intvariable;
+        char charvariable;
+        Operators operatorID;
+    };
+};
+
+ErrorCode GetToken(string file, int& i, FILE out, token& tokeru);
+void Write(string file, token& tokeru, FILE& out);
+ErrorCode GetComment(string file, int& i, FILE&out, token& tokeru);
+void Map();
+
+#endif // TOKEN_H
